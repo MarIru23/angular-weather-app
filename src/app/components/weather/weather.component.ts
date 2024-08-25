ee466997701f5458db8ec2d1d2a0a03b8ce4e3c1
Mario Iruela <marioiruelaro@gmail.com>
2024-08-26 01:33:06 +0200

    feat: images and organized code

--- src/app/components/weather/weather.component.ts
+++ src/app/components/weather/weather.component.ts
@@ -7,59 +7,57 @@ import { WeatherInterface } from '../../interfaces/weather.interface';
   standalone: true,
   imports: [],
   templateUrl: './weather.component.html',
-  styleUrl: './weather.component.css'
+  styleUrl: './weather.component.css',
 })
-export class WeatherComponent implements OnInit{
-  
-  weatherList: WeatherInterface | undefined;
-  city : string ='';
+export class WeatherComponent implements OnInit {
+  weatherList!: WeatherInterface;
+  city: string = '';
+  url: string = '';
+  api_key = '62ff94c8139aae06b3d3e0c029d6c116';
+  imageUrl = 'assets/images/';
 
-  constructor(private weatherService : WeatherService){}
-  
-  ngOnInit(): void {
-  }
+  constructor(private weatherService: WeatherService) {}
+
+  ngOnInit(): void {}
 
   onSubmit(event: Event, city: string) {
     event.preventDefault();
-    this.getCity(city);
-  }
-    
-
-  getCity(city:string) {
-    this.city=city;
-    this.weatherService.changeUrl(this.city);
+    this.city = city;
     this.getWeather();
   }
 
   getWeather() {
-    this.weatherService.getWeather().subscribe({
+    this.url = `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.api_key}`;
+    this.weatherService.getWeather(this.url).subscribe({
       next: (result) => {
         if (result) {
           this.weatherList = result;
-          this.convertTemperatures(this.weatherList?.main);
-          this.convertTimestampToUTC(this.weatherList?.sys);
+          this.weatherList.weather[0].icon =
+            this.imageUrl + this.weatherList.weather[0].icon + '.png';
+          this.convertTemperatures(this.weatherList.main);
+          this.convertTimestampToUTC(this.weatherList.sys);
         }
       },
       error: (err) => {
         console.log(err);
-      }
+      },
     });
   }
 
-  private convertTimestampToUTC(main:any){
+  private convertTimestampToUTC(main: any) {
     const dateR = new Date(main.sunrise * 1000);
     const dateS = new Date(main.sunset * 1000);
     main.sunrise = dateR.toLocaleString('en-GB', { timeStyle: 'short' });
     main.sunset = dateS.toLocaleString('en-GB', { timeStyle: 'short' });
   }
-  
+
   private convertTemperatures(main: any) {
-    const kelvinToCelsius = (temp: number) => parseFloat((temp - 273.15).toFixed(0));
-  
+    const kelvinToCelsius = (temp: number) =>
+      parseFloat((temp - 273.15).toFixed(0));
+
     main.temp = kelvinToCelsius(main.temp);
     main.feels_like = kelvinToCelsius(main.feels_like);
     main.temp_max = kelvinToCelsius(main.temp_max);
     main.temp_min = kelvinToCelsius(main.temp_min);
   }
-
 }
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -3,21 +3,12 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WeatherService {
+  constructor(private httpClient: HttpClient) {}
 
-  new_url='';
-
-  constructor(private httpClient : HttpClient) {
-   }
-   changeUrl(city : string):string{
-    return this.new_url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=62ff94c8139aae06b3d3e0c029d6c116`;
- }
-
-   getWeather(): Observable<any>{
-    return this.httpClient.get(this.new_url);
+  getWeather(url: string): Observable<any> {
+    return this.httpClient.get(url);
   }
-
- 
 }
